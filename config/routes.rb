8ad14ba70c5dc5235be7aa4f6c7f522728cbd019
Yoichi Hirai <yhirai@yugen.org>
2009-03-14 16:14:43 +0900

    remove ambiguous route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ ActionController::Routing::Routes.draw do |map|
   # Restful Authentication Rewrites
   map.logout '/logout', :controller => 'sessions', :action => 'destroy'
   map.login '/login', :controller => 'sessions', :action => 'new'
-  map.login '/write', :controller => 'patches', :action => 'new'
   map.register '/register', :controller => 'users', :action => 'create'
   map.signup '/signup', :controller => 'users', :action => 'new'
   map.activate '/activate/:activation_code', :controller => 'users', :action => 'activate', :activation_code => nil
