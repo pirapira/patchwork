034c97184bb0786dc43fcb24352884e8725fdde4
Yoichi Hirai <yhirai@yugen.org>
2009-03-14 22:40:10 +0900

    maybe a deadend

--- app/models/patch.rb
+++ app/models/patch.rb
@@ -3,8 +3,11 @@ class Patch < ActiveRecord::Base
   # Relationships
   belongs_to :user
   belongs_to :parent, :class_name => "Patch", :foreign_key => "parent_id"
-  has_many :pre, :through => :prepost
-  has_many :post, :through => :prepost
+  has_many :preposts, :foreign_key => 'pre_id'
+  has_many :preposts, :foreign_key => 'post_id'
+  
+  has_many :pre, :through => :preposts
+  has_many :post, :through => :preposts
 
   # Validations
   validates_presence_of :user
