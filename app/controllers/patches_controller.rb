ea927cbdad7b352455de2e1475817c81a77ef02b
Yoichi Hirai <yhirai@yugen.org>
2009-03-14 20:03:03 +0900

    adding parent patch

--- app/controllers/patches_controller.rb
+++ app/controllers/patches_controller.rb
@@ -2,22 +2,62 @@ class PatchesController < ApplicationController
 #  require_role "subscriber", :for => :new
   
   def new
-    @patch = Patch.new
+    if logged_in?
+      @patch = Patch.new
+    else
+      flash[:error] = 'Please login first'
+      redirect_back_or_default(root_path)
+      return
+    end
   end
 
   def create
     @patch = Patch.new(params[:patch])
+    if current_user.nil?
+      flash[:error] = 'Please login first'
+      redirect_back_or_default(root_path)
+      return
+    end
     @patch.user = current_user
+    @patch.parent_id = nil
     @patch.save!
     flash[:notice] = "saved your patch"
     redirect_back_or_default(root_path)
+  end
+  
+  def edit
+    if current_user.nil?
+      flash[:error] = 'Please login first'
+      redirect_back_or_default(root_path)
+      return
+    end
+    @patch = Patch.find(params[:id])
+    render :action => "new"
   end
 
   def show
     @patch = Patch.find(params[:id])
   end
-  
+
   def index
     @patches = Patch.find(:all)
+  end
+
+  def update
+    if current_user.nil?
+      flash[:error] = 'Please login first'
+      redirect_back_or_default(root_path)
+      return
+    end
+    @patch = Patch.new(params[:patch])
+    @patch.user = current_user
+    if old_version = Patch.find(@patch.id)
+      @patch.parent_id = old_version.id
+    else
+      raise "invalid"
+    end
+    @patch.save!
+    flash[:notice] = "saved your patch"
+    redirect_back_or_default(root_path)
   end
 end
--- app/models/patch.rb
+++ app/models/patch.rb
@@ -2,6 +2,7 @@ class Patch < ActiveRecord::Base
   
   # Relationships
   belongs_to :user
+  belongs_to :parent, :class_name => "Patch", :foreign_key => "parent_id"
 
   # Validations
   validates_presence_of :user
