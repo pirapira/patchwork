650ca1534000597b6236660d13b1ca5786f49fa2
Yoichi Hirai <yhirai@yugen.org>
2009-03-14 14:19:41 +0900

    generate model patch

--- config/environment.rb
+++ config/environment.rb
@@ -57,8 +57,8 @@ Rails::Initializer.run do |config|
   # Make sure the secret is at least 30 characters and all random, 
   # no regular words or you'll be exposed to dictionary attacks.
   config.action_controller.session = {
-    :session_key => '_bort_session',
-    :secret      => 'a3e2a51a371bb964a4250c21f8d083f9ddb224d455171dcba55518e74af43366e52e3f239773f90aed0ab6caf6554f051504ce7232599d066150dbabff0f1654'
+    :session_key => '_patchwork_session',
+    :secret      => 'dajogoeadakkskkdaj3i48adjafi8e3kjgajfdoalkkfdgjaidalea;vaxolsddlasagfjeae;alksajcaielkdaj'
   }
 
   # Use the database for sessions instead of the cookie-based default,
