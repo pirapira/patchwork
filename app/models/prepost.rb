544a702efd51d570c9634bba25c424caf971ee91
Yoichi Hirai <yhirai@yugen.org>
2009-03-15 00:28:40 +0900

    tiny changes

--- app/models/prepost.rb
+++ app/models/prepost.rb
@@ -1,5 +1,11 @@
 class Prepost < ActiveRecord::Base
+
+  # Relationships
   belongs_to :user, :dependent => :destroy
   belongs_to :prepatch, :class_name => "patch", :dependent => :destroy
   belongs_to :postpatch, :class_name => "patch", :dependent => :destroy
+
+  # Validations
+  validates_presence_of :prepatch
+  validates_presence_of :postpatch
 end
