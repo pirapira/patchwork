a58c986122f1b886705ac92ff0cacfb86a9bd63e
Yoichi Hirai <yhirai@yugen.org>
2009-03-15 21:07:44 +0900

    login simplified

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
   skip_before_filter :verify_authenticity_token, :only => :create
-  
+
   def new
   end
 
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
     if using_open_id?
       open_id_authentication
     else
-      password_authentication
+      note_failed_login
     end
   end
 
@@ -18,11 +18,21 @@ class SessionsController < ApplicationController
     flash[:notice] = "You have been logged out."
     redirect_back_or_default(root_path)
   end
-  
+
   def open_id_authentication
-    authenticate_with_open_id do |result, identity_url|
-      if result.successful? && self.current_user = User.find_by_identity_url(identity_url)
-        successful_login
+    authenticate_with_open_id(params[:openid_url],
+                                :required => [:nickname, :email]) do
+        |result, identity_url, registration|
+      if result.successful? then
+        if self.current_user = User.find_by_identity_url(identity_url) then
+          successful_login
+        else
+          create_new_user(:identity_url => identity_url,
+                               :login => registration['nickname'],
+                          :email => registration['email'])
+          self.current_user = User.find_by_identity_url(identity_url)
+          successful_login
+        end
       else
         flash[:error] = result.message || "Sorry no user with that identity URL exists"
         @remember_me = params[:remember_me]
@@ -32,20 +42,7 @@ class SessionsController < ApplicationController
   end
 
   protected
-  
-  def password_authentication
-    user = User.authenticate(params[:login], params[:password])
-    if user
-      self.current_user = user
-      successful_login
-    else
-      note_failed_signin
-      @login = params[:login]
-      @remember_me = params[:remember_me]
-      render :action => :new
-    end
-  end
-  
+
   def successful_login
     new_cookie_flag = (params[:remember_me] == "1")
     handle_remember_cookie! new_cookie_flag
@@ -53,8 +50,30 @@ class SessionsController < ApplicationController
     flash[:notice] = "Logged in successfully"
   end
 
-  def note_failed_signin
+  def note_failed_login
     flash[:error] = "Couldn't log you in as '#{params[:login]}'"
     logger.warn "Failed login for '#{params[:login]}' from #{request.remote_ip} at #{Time.now.utc}"
   end
+
+  def create_new_user(attributes)
+    @user = User.new(attributes)
+    if @user && @user.valid?
+      @user.register_openid!
+    end
+
+    if @user.errors.empty?
+      new_cookie_flag = (params[:remember_me] == "1")
+      handle_remember_cookie! new_cookie_flag
+      @user.roles << Role.find(:first,
+                               :conditions => [ "name = ?",  'authenticated'])
+    else
+      failed_creation
+    end
+  end
+
+  def failed_creation(message = 'Sorry, there was an error creating your account')
+    flash[:error] = message
+    render :action => :new
+  end
+
 end
